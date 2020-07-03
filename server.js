346b13b0c202e365289dbedb651c116b14ec4e8e
Anna Jose <joann23july@gmail.com>
2020-07-03 16:20:42 -0400

    created route files

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const connectDB = require('./db');
 connectDB();
 
 app.get('/', (req,res) =>res.send('API Running'));
+app.use('/api/users',require('./routes/users'));
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/profile',require('./routes/profile'));
+app.use('/api/post',require('./routes/posts'));
 
 const PORT = process.env.port || 80;
 app.listen(PORT, ()=>console.log(`Server started in port ${PORT} `));
