db508697c6ade7d55670eb80ff0ff16c65d5281d
Anna Jose <joann23july@gmail.com>
2020-07-04 17:22:30 -0400

    jwt

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../models/User');
 const {body,validationResult} = require('express-validator');
 // @route GET api/users
@@ -41,7 +43,22 @@ router.post('/',[
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password,salt);
         await user.save();
-        res.send('User registered');
+        const payload = {
+            user : {
+                id : user.id
+            }
+        };
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {expiresIn: 360000},
+             (err,token) => {
+                 if(err) throw err;
+                 res.json({token})
+             }
+            );
+
+
     }catch(err){
         console.error(err.message);
         res.send(500).send('Server error')
